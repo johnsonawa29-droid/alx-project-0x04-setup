b7e16b926680ebb29fb8df6b30c8748ca354a595
johnson <johnsonawa29@gmail.com>
2025-10-04 03:36:04 +0100

    yes

---
+++ alx-project-0x05/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="w-full p-4 bg-gray-100 shadow-md">
+      <h1 className="text-xl font-bold">My App Header</h1>
+    </header>
+  );
+};
+
+export default Header;
